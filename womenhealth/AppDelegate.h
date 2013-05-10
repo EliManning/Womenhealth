376a1e47398ffe80fcc7286fed8f18cf92defe88
ErLi <lier0715@bu.edu>
2013-05-09 22:53:06 -0400

    Initial Commit

---
+++ womenhealth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  womenhealth
+//
+//  Created by smart_parking on 5/9/13.
+//  Copyright (c) 2013 codes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
